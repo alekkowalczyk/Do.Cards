e8f92d6eadbf7f646d0388f8886be178c37b46f4
Alek Kowalczyk <alek.kowalczyk.7@gmail.com>
2016-11-28 16:47:15 +0100

    forgot

--- src/client/card/actions/cardGroupActionConstants.ts
+++
@@ -1,5 +0,0 @@
-export type ADD_CARD = "CardGroup/ADD_CARD";
-export const ADD_CARD: ADD_CARD = "CardGroup/ADD_CARD";
-
-export type ARCHIVE_CARD = "CardGroup/ARCHIVE_CARD";
-export const ARCHIVE_CARD: ARCHIVE_CARD = "CardGroup/ARCHIVE_CARD";
